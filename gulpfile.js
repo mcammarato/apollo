9bf20d489d5cf19862265e5f6e82265a4db525c4
Mike Cammarato <mike@Mikes-MBP.home>
2017-07-05 00:07:15 -0400

    Updated project to use handlebars for templating and changes server config

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,62 @@
 var gulp        = require('gulp'),
     watch       = require('gulp-watch'),
     sass        = require('gulp-sass'),
-    bs          = require('browser-sync').create();
+    server      = require('gulp-develop-server'),
+    bs          = require('browser-sync'),
+    rename      = require('gulp-rename');
 
-// Convert SCSS to CSS
+
+// Compile SCSS to CSS
 gulp.task('styles', function () {
-  return gulp.src('client/app/scss/**/*.scss')
+  return gulp.src('client/assets/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('client/app/css'));
+    .pipe(gulp.dest('client/assets/css'));
 });
+ 
 
-// Browser Sync
+// Browser Sync Init Options
+var options = {
+    server: {
+        path: 'server.js',
+        execArgv: [ '--harmony' ]
+    },
+    bs: {
+        proxy: 'http://localhost:3000',
+        port: '6300',
+        notify: false,
+        injectChanges: true,
+        reloadOnRestart: true
+    }
+};
+
+// Browser sync
 gulp.task('browser-sync', function() {
       bs.init({
-      proxy: "localhost:3000",
-      notify: false,
-      injectChanges: true
     });
-
 });
+
+// Server start
+gulp.task('server:start', function() {
+    server.listen( options.server, function( error ) {
+        if (!error) bs(options.bs);
+    });
+});
+
+// Server restart
+gulp.task('server:restart', function() {
+    server.restart( function( error ) {
+        if(!error) bs.reload();
+    });
+});
+
 
 // Watch Task
-gulp.task('watch', ['browser-sync'], function () {
-    gulp.watch('client/app/scss/**/*.scss',['styles']);
+gulp.task('watch', ['server:start'], function () {
+    gulp.watch('client/assets/scss/**/*.scss',['styles']);
     gulp.watch('client/**/*.html').on('change', bs.reload);
-    gulp.watch('client/app/css/**/*.css').on('change', bs.reload);
+    gulp.watch('client/assets/css/**/*.css').on('change', bs.reload);
+    gulp.watch('client/views/**/*.hbs').on('change', bs.reload);
+    gulp.watch('client/views/layouts/**/*.hbs').on('change', bs.reload);
+    gulp.watch('client/views/partials/**/*.hbs').on('change', bs.reload);
 });
+
--- server.js
+++ server.js
@@ -1,41 +1,88 @@
 var express     = require('express'),
+    path        = require('path'),
+    hbs         = require('express-handlebars'),
     app         = express();
 
-// Css
-app.use('/css', express.static(__dirname + '/client/app/css'));
 
+/// Express-Handlebars Config ///
+////////////////////////////////
+
+app.set('view engine', 'hbs');
+
+app.set('views', 'client/views/partials');
+
+app.engine('hbs', hbs({
+        defaultLayout: 'index', 
+        extname: '.hbs',
+        layoutsDir:'client/views/layouts',
+        partialsDir: 'client/views/partials'
+}));
+
+
+
+/// Client Side Assets ///
+/////////////////////////
+
+
+// CSS
+app.use('/css', express.static(__dirname + '/client/assets/css'));
+// SCSS
+app.use('/scss', express.static(__dirname + '/client/assets/scss'));
 // JS
-app.use('/js', express.static(__dirname + '/client/app/js'));
-
+app.use('/js', express.static(__dirname + '/client/assets/js'));
 // Images
-app.use('/images', express.static(__dirname + '/client/app/images'));
-
+app.use('/images', express.static(__dirname + '/client/assets/images'));
 // Fonts
-app.use('/fonts', express.static(__dirname + '/client/app/fonts'));
+app.use('/fonts', express.static(__dirname + '/client/assets/fonts'));
 
+
+/// Node Moduels ///
+///////////////////
+
+
+// jQuery
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+// Foundation CSS
+app.use('/css', express.static(__dirname + '/node_modules/foundation-sites/dist/css'));
+// Foundation JS
+app.use('/js', express.static(__dirname + '/node_modules/foundation-sites/dist/js'));
+// Slick Carousel
+app.use('/slick', express.static(__dirname + '/node_modules/slick-carousel/slick'));
+// Dropit
+app.use('/dropit', express.static(__dirname + '/node_modules/jb-dropit'));
 // Font Awesome Icons
 app.use('/font-awesome', express.static(__dirname + '/node_modules/font-awesome/css'));
-
-// Slick Carousel
-app.use('/slick', express.static(__dirname + '/client/app/assets/slick'));
-
-// Dropit
-app.use('/dropit', express.static(__dirname + '/client/app/assets/dropit'));
-
 // Browser-Sync
 app.use('/browser-sync', express.static(__dirname + '/node_modules/browser-sync/bin'));
+
+
+/// Server Routes ///
+///////////////////
+
 
 // Root Index
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html');
 });
 
-// Test Page
-app.get('/demo', function(req, res) {
-  res.sendFile(__dirname + '/client/demo.html');
+// Componets Page
+app.get('/componets', function(req, res) {
+  res.sendFile(__dirname + '/client/componets.html');
+});
+
+// HBS Testing
+app.get('/hbs', function(req, res) {
+  res.render('header');
+});
+
+// 404
+app.get('/404', function(req, res) {
+  res.sendFile(__dirname + '/client/404.html');
 });
 
 // Server
-app.listen(3000, function() {
-  console.log('Listening on 3000');
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on ' + app.get('port'));
 });
