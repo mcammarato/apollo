299414c804da37ca28d512ea9deaa40e67c4087a
Mike Cammarato <mike@Mikes-MBP.home>
2017-12-14 23:07:52 -0500

    Gulpfile fixes, updated readme

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Getting started:
 1. Clone the repo or download, unzip, and navigate to the folder in terminal or command prompt.
 2. Run 'npm install' or 'yarn --production' (if you're using yarn) to download server side dependencies.
 3. Run 'bower install' to download client side dependencies.
-4. Run 'gulp watch' to compile SCSS, LESS, and start BrowserSync.
-5. Enjoy :)
+4. Run 'node server' to start the server
+5. Run 'gulp watch' to start browsersync
+6. Enjoy :)
--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,7 @@ var gulp        = require('gulp'),
     watch       = require('gulp-watch'),
     sass        = require('gulp-sass'),
     less        = require('gulp-less'),
-    path        = require('path'),
-    server      = require('gulp-develop-server'),
-    bs          = require('browser-sync'),
-    saveFile    = require('gulp-savefile'),
+    bs          = require('browser-sync').create(),
     cleanCSS    = require('gulp-clean-css'),
     concat      = require('gulp-concat'),
     uglify      = require('gulp-uglify'),
@@ -24,7 +21,8 @@ gulp.task('scss', function () {
 gulp.task('less', function() {
   return gulp.src('client/assets/less/**/*.less')
     .pipe(less())
-    .pipe(gulp.dest('client/assets/css'));
+    .pipe(gulp.dest('client/assets/css'))
+    .pipe(bs.reload({stream: true}));
 });
 
 
@@ -37,7 +35,8 @@ gulp.task('minify-css', function(){
     .pipe(gulp.dest('client/dist'))
     .pipe(rename('bundle.min.css'))
     .pipe(sourceMaps.write())
-    .pipe(gulp.dest('client/dist'));
+    .pipe(gulp.dest('client/dist'))
+    .pipe(bs.reload({stream: true}));
 });
 
 // Uglify JS
@@ -49,62 +48,30 @@ gulp.task('uglify-js', [], function() {
       .pipe(rename('bundle.min.js'))
       .pipe(uglify())
       .pipe(sourceMaps.write())
-      .pipe(gulp.dest('client/dist'));
+      .pipe(gulp.dest('client/dist'))
+      .pipe(bs.reload({stream: true}));
 });
 
-// Browser Sync Init Options
-var options = {
-    server: {
-        path: 'server.js',
-        execArgv: [ '--harmony' ]
-    },
-    bs: {
-        proxy: 'http://localhost:3000',
-        port: '6300',
+// Browsersync
+gulp.task('browser-sync', function() {
+    bs.init({
         notify: false,
         injectChanges: true,
-        reloadOnRestart: true
-    }
-};
-
-// Browser sync
-gulp.task('browser-sync', function() {
-      bs.init({
+        proxy: {
+          target: 'localhost:3000'
+        }
     });
 });
-
-// Server start
-gulp.task('server:start', function() {
-    server.listen( options.server, function( error ) {
-        if (!error) bs(options.bs);
-    });
-});
-
-// Server restart
-gulp.task('server:restart', function() {
-    server.restart( function( error ) {
-        if(!error) bs.reload();
-    });
-});
-
 
 
 // Watch Task
-gulp.task('watch', ['server:start'], function () {
+gulp.task('watch', ['browser-sync'], function () {
     gulp.watch('client/assets/scss/**/*.scss',['scss']);
     gulp.watch('client/assets/less/**/*.less',['less']);
     gulp.watch('client/assets/js/**/*.js', ['uglify-js']);
     gulp.watch('client/assets/css/**/*.css',['minify-css']);
-    gulp.watch('client/assets/bootstrap/less/**/*.less').on('change', function(){
-      return gulp.src('client/assets/less/bootstrap-custom.less').pipe(saveFile());
-    });
-    gulp.watch('client/**/*.html').on('change', bs.reload);
-    gulp.watch('client/assets/css/**/*.css').on('change', bs.reload);
-    gulp.watch('client/assets/less/**/*.less').on('change', bs.reload);
-    gulp.watch('client/assets/scss/**/*.scss').on('change', bs.reload);
-    gulp.watch('client/dist/**/*.css').on('change', bs.reload);
-    gulp.watch('client/dist/**/*.js').on('change', bs.reload);
     gulp.watch('client/views/**/*.hbs').on('change', bs.reload);
     gulp.watch('client/views/layouts/**/*.hbs').on('change', bs.reload);
     gulp.watch('client/views/partials/**/*.hbs').on('change', bs.reload);
+    gulp.watch('client/views/**/*.hbs').on('change', bs.reload);
 });
